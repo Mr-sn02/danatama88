e1ea42a5339fe86a560a60a53cb807e65d9fd0cd
mr sn <sonnnn603@gmail.com>
2025-12-01 20:10:05 +0700

    Create products.js

---
+++ danatama_next_minimal/lib/products.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    name: "Saham BBCA",
+    price: 35000,
+    description:
+      "Bank Central Asia Tbk – saham bluechip perbankan dengan likuiditas tinggi."
+  },
+  {
+    id: 2,
+    name: "Saham BBRI",
+    price: 5100,
+    description:
+      "Bank Rakyat Indonesia – fokus pada segmen mikro & UMKM dengan basis nasabah luas."
+  },
+  {
+    id: 3,
+    name: "Saham TLKM",
+    price: 3900,
+    description:
+      "Telkom Indonesia – emiten telekomunikasi terbesar dengan eksposur digital."
+  },
+  {
+    id: 4,
+    name: "Reksa Dana Pasar Uang",
+    price: 100000,
+    description:
+      "Instrumen risiko rendah berbasis deposito & surat utang jangka pendek, cocok untuk dana parkir."
+  },
+  {
+    id: 5,
+    name: "Obligasi Negara Ritel",
+    price: 1000000,
+    description:
+      "Simulasi instrumen pendapatan tetap dengan kupon berkala dari pemerintah Indonesia."
+  }
+];
+
+export default products;
